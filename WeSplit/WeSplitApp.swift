1f771970834ca89e54a7e5e1f4520c8c02d49ff7
Conrad Taylor <conradwt@gmail.com>
2022-07-22 21:34:48 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Conrad Taylor on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
